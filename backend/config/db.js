56dbf7a0a261158fb13066b78742f650aa1ead96
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-03-01 21:25:15 +0530

    created signup and login api, jwt , cookie added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
-const connectDB= async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Sucesfully connected to mongoDB😊");
-    } catch(error) {
-        console.log(`ERROR: ${error.message}`)
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB connection established`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,20 @@
-// packages
-import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js"; // Ensure .js extension
 
-// Utils
-import connectDB from './config/db.js'
+import connectDB from "./config/db.js";
+
 dotenv.config();
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
-const app=express();
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req,res)=>{
-    res.send("Hello World!");
-})
+app.use("/api/users", userRoutes);
 
-app.listen(port,()=>console.log(`Server running on port : ${port}`));
+app.listen(port, () => console.log(`Server running on port : ${port}`));
