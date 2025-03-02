13c1f48079ebe332fa229d9b62565f5400f9df5e
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-03-02 18:14:06 +0530

    created userList fro admin route and its functionalities

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { Profiler, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import React from 'react'
@@ -9,15 +9,34 @@ import { createBrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 
+// Private Route
+import PrivateRoute from './components/PrivateRoute.jsx'
+
 // Auth
 import Login from './pages/Auth/Login.jsx'
 import Register from './pages/Auth/Register.jsx'
+
+import Profile from './pages/User/Profile.jsx'
+import AdminRoute from './pages/Admin/AdminRoute.jsx'
+
+import UserList from './pages/Admin/UserList.jsx'
 
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
+      <Route path='' element={<PrivateRoute/>}>
+      <Route path='/profile' element={<Profile/>}/>
+      </Route>
+      
+
+      
+      <Route path='/admin' element={<AdminRoute/>}>
+      <Route path='userlist' element={<UserList/>}/>
+      </Route>
+
+      
       </Route>
   )
 )
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import flowbitePlugin from"flowbite/plugin"
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
+    flowbitePlugin(),
   ],
   server:{
     proxy:{
