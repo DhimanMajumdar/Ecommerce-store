35ed5c49e4e7d8d219fde105f002045696b10d50
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-03-02 19:15:51 +0530

    made category apis at backend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js"; // Ensure .js extension
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 import connectDB from "./config/db.js";
 
 dotenv.config();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -16,5 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
+app.use("/api/category",categoryRoutes);
 
 app.listen(port, () => console.log(`Server running on port : ${port}`));
